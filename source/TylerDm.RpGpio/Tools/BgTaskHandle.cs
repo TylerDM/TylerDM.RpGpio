353fae389606ed9e16507b867e87b315683084a0
Tyler Mays <16845057+TylerDM@users.noreply.github.com>
2024-12-29 13:23:08 -0500

    Copied updated version from other lib until I update the reference.

--- source/TylerDm.RpGpio/Tools/BgTaskHandle.cs
+++ source/TylerDm.RpGpio/Tools/BgTaskHandle.cs
@@ -3,10 +3,30 @@
 public class BgTaskHandle : IDisposable
 {
 	private readonly CancellationTokenSource _cts = new();
+	private readonly Task _task;
+
+	private bool disposed = false;
 
 	public BgTaskHandle(Func<CancellationToken, Task> func) =>
-		Task.Run(() => func(_cts.Token));
+		_task = Task.Run(() => func(_cts.Token));
 
-	public void Dispose() =>
+	public BgTaskHandle(Action<CancellationToken> action) =>
+		_task = Task.Run(() => action(_cts.Token));
+
+	/// <summary>
+	/// Wait for the task to complete.
+	/// </summary>
+	public Task WaitAsync(CancellationToken ct = default) =>
+		_task.WaitAsync(ct);
+
+	/// <summary>
+	/// Cancels the background task and disposes unmanaged internal resources.
+	/// </summary>
+	public void Dispose()
+	{
+		if (disposed) return;
+		disposed = true;
+
 		_cts.Dispose();
+	}
 }
