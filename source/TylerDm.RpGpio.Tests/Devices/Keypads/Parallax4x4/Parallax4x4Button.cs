4b463452b3fefbaa3a59a28b2fd514b1da422a78
Tyler Mays <16845057+TylerDM@users.noreply.github.com>
2025-01-06 18:56:59 -0500

    Disable tests for now.

---
+++ source/TylerDm.RpGpio.Tests/Devices/Keypads/Parallax4x4/Parallax4x4Button.cs
@@ -0,0 +1,21 @@
+﻿namespace TylerDm.RpGpio.Devices.Keypads.Parallax4x4;
+
+//Sorry, not dealing with this right now.
+//public class Parallax4x4Button
+//{
+//	private static readonly TimeSpan _pressDuration = TimeSpan.FromMilliseconds(5);
+
+//	public bool Pressed => outputPin.Read();
+
+//	public Parallax4x4Button(TestingPin inputPin, TestingPin outputPin)
+//	{
+		
+//	}
+
+//	public async void Press()
+//	{
+		
+//		await _pressDuration.WaitAsync();
+//		Pressed = false;
+//	}
+//}
