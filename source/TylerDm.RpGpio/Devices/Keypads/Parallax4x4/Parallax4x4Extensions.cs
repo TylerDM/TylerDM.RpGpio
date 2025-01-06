e2bbe2ddefcc90f999e90621446f89c46b6a5987
Tyler Mays <16845057+TylerDM@users.noreply.github.com>
2025-01-06 18:58:17 -0500

    Add ReadUntilCancelAsync().

--- source/TylerDm.RpGpio/Devices/Keypads/Parallax4x4/Parallax4x4Extensions.cs
+++ source/TylerDm.RpGpio/Devices/Keypads/Parallax4x4/Parallax4x4Extensions.cs
@@ -4,6 +4,29 @@ public static class Parallax4x4Extensions
 {
 	private const PinReadModes _readMode = PinReadModes.InputPullDown;
 
+	public static async Task<string> ReadUntilCancelAsync(this Parallax4x4 keypad, CancellationToken ct, Action<char>? onKeyPressed = null)
+	{
+		var list = new List<char>(10);
+
+		void handleKeyPressed(char ch)
+		{
+			list.Add(ch);
+
+			onKeyPressed?.Invoke(ch);
+		}
+
+		try
+		{
+			keypad.OnKeyPressed += handleKeyPressed;
+			await ct.WaitForCancelAsync(CancellationToken.None);
+			return new([.. list]);
+		}
+		finally
+		{
+			keypad.OnKeyPressed -= handleKeyPressed;
+		}
+	}
+
 	public static Parallax4x4 OpenParallax4x4(this IGpio gpio,
 			PinNumber p0w, PinNumber p1w, PinNumber p2w, PinNumber p3w,
 			PinNumber p4r, PinNumber p5r, PinNumber p6r, PinNumber p7r
